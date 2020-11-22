a46dea86471ae7179f76a16bc29acdb9c79ec2c8
duivesteyn <duivesteyn@gmail.com>
2020-11-22 22:33:52 +1030

    bugfix

--- updateDisplay.py
+++ updateDisplay.py
@@ -20,16 +20,64 @@
 #v1.2 - API Changed from CME to yahoo finance
 
 # Available Properties 
-# "last":"28.01",
-# "change":"+0.45"
-# "priorSettle":"27.56",
-# "open":"27.64",
-# "close":"-",
-# "high":"28.75"
-# "low":"27.24
-# "volume":"13,426"
-# "percentageChange":"+1.63%"
-# "escapedQuoteCode":"CLM0"
+
+# "language":"en-US"
+# "region":"US"
+# "quoteType":"FUTURE"
+# "triggerable":false
+# "headSymbolAsString":"CL=F"
+# "contractSymbol":false
+# "currency":"USD"
+# "marketState":"REGULAR"
+# "underlyingSymbol":"CLF21.NYM"
+# "underlyingExchangeSymbol":"CLF21.NYM"
+# "openInterest":427104
+# "expireDate":1608508800
+# "expireIsoDate":"2020-12-21T00:00:00Z"
+# "fiftyDayAverage":39.908855
+# "fiftyDayAverageChange":2.5611458
+# "fiftyDayAverageChangePercent":0.064174876
+# "twoHundredDayAverage":38.764088
+# "twoHundredDayAverageChange":3.7059135
+# "twoHundredDayAverageChangePercent":0.09560172
+# "sourceInterval":30
+# "exchangeDataDelayedBy":30
+# "exchangeTimezoneName":"America/New_York"
+# "exchangeTimezoneShortName":"EST"
+# "gmtOffSetMilliseconds":-18000000
+# "esgPopulated":false
+# "tradeable":false
+# "firstTradeDateMilliseconds":967003200000
+# "regularMarketChange":0.5699997
+# "regularMarketChangePercent":1.3603811
+# "regularMarketTime":1605909599
+# *** "regularMarketPrice":42.47 ***
+# "regularMarketDayHigh":42.54
+# "regularMarketDayRange":"41.61 - 42.54"
+# "regularMarketDayLow":41.61
+# "regularMarketVolume":263618
+# "regularMarketPreviousClose":41.9
+# "bid":42.42
+# "ask":42.6
+# "bidSize":9
+# "askSize":8
+# "exchange":"NYM"
+# "market":"us24_market"
+# "fullExchangeName":"NY Mercantile"
+# "shortName":"Crude Oil Jan 21"
+# "regularMarketOpen":41.88
+# "averageDailyVolume3Month":339797
+# "averageDailyVolume10Day":234997
+# "fiftyTwoWeekLowChange":82.79
+# "fiftyTwoWeekLowChangePercent":-2.0533235
+# "fiftyTwoWeekRange":"-40.32 - 65.65"
+# "fiftyTwoWeekHighChange":-23.18
+# "fiftyTwoWeekHighChangePercent":-0.35308453
+# "fiftyTwoWeekLow":-40.32
+# "fiftyTwoWeekHigh":65.65
+# "priceHint":2
+# "symbol":"CL=F"
+
 
 #--------------------------------------------------
 #|                                     2020-05-23 | 
@@ -85,18 +133,19 @@ strLast = '$' +  str(newData['regularMarketPrice'])
 strChange = str(newData['regularMarketChangePercent'])
 strH = 'H:' + str(newData['regularMarketDayHigh']) + " "
 strL = 'L:' + str(newData['regularMarketDayLow'])  + " "
+strQuoteCode = str(newData['underlyingSymbol'])
 
 # Write text                
 draw.text((  3,  3), 'OLJEPRISEN' +  "   ", inky_display.BLACK, font=fontLg)
 draw.text((141,  1), datetime.today().strftime('%Y-%m-%d')     + "   ", inky_display.BLACK, font=font) 
 x=draw.textsize(datetime.today().strftime('%Y-%m-%d'), font)[0]-draw.textsize(datetime.today().strftime('%H:%M'), font)[0]
 draw.text((141+x+4, 10), datetime.today().strftime('%H:%M')+ "   ", inky_display.BLACK, font=font)          
-draw.text(( 5, 30) , strLast, inky_display.BLACK, font=fontExLg)                                                 #Price, #33.56
-draw.text((160, 40), strH  , inky_display.BLACK, font=font)                                                    #High, 34.00
-draw.text((160, 50), strL  , inky_display.BLACK, font=font)                                                    #Low,  30.72
+draw.text(( 5, 30) , strLast, inky_display.BLACK, font=fontExLg)                                                #Price, #33.56
+draw.text((160, 40), strH  , inky_display.BLACK, font=font)                                                     #High, 34.00
+draw.text((160, 50), strL  , inky_display.BLACK, font=font)                                                     #Low,  30.72
 xDistanceToStartPercentageChange=1+draw.textsize(strLast, fontExLg)[0]-draw.textsize(strChange, fontLg)[0]-3    #Right Aligning %Change and Last Price
 draw.text((xDistanceToStartPercentageChange, 70), str(strChange) + " ", inky_display.BLACK, font=fontLg)        #Change, -5.31%. Right Aligned with the last price
-draw.text((1, 94), str(newData['quoteCode']) +" ", inky_display.BLACK, font=font)                               #Footer: CLX
+draw.text((1, 94), strQuoteCode +" ", inky_display.BLACK, font=font)                                            #Footer: CLX
 
 # Write to Display
 inky_display.set_border(colour)
